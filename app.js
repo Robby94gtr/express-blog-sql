64869e9ee337c205e2729f63ff8bf12c3bd896d5
Robby94gtr <robertogramazio94@gmail.com>
2025-06-03 17:55:03 +0200

    [UPD] Milestone 1

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const port = 3000
 const blogRouter = require("./routers/blog")
+const mysql = require('./data/db')
 app.use(express.static('public'));
 
 app.use(express.json());
---
+++ data/db.js
@@ -0,0 +1,17 @@
+const mysql = require('mysql2')
+
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'federsa65',
+    database: 'db_blog',
+});
+
+connection.connect((err) => {
+    if (err) { console.log(err); }
+    else {
+        console.log('Connected to my sql!');
+    }
+})
+
+module.exports = connection
