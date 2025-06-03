ed5f03507c394c77b80e48c061cce6fbd4f8d3be
Robby94gtr <robertogramazio94@gmail.com>
2025-06-03 17:59:30 +0200

    [UPD] Defining 3rd Milestone

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,8 +1,8 @@
-//recupero la variabile di connessioneAdd commentMore actions
+//recupero la variabile di connessione
 const connection = require('../data/db')
 
 const index = (req, res) => {
-    //preparo la query per recuperare i blog dal dbAdd commentMore actions
+    //preparo la query per recuperare i blog dal db
     const sql = "SELECT * FROM posts";
     //eseguo la query
     connection.query(sql, (err, results) => {
@@ -17,7 +17,19 @@ const show = (req, res) => {
     res.send("post con id: " + req.params.id)
 }
 
+const destroy = (req, res) => {
+    //recupero id
+    const id = req.params.id;
+    // query
+    connection.query("DELETE FROM posts WHERE id = ?", [id], (err) => {
+        //funzione di callback ha solo un parametro perché destroy deve restituire il codice di stato 204
+        if (err) return res.status(500).json({ error: "Database query failed" + err });
+    })
+
+}
+
 module.exports = {
     index,
-    show
+    show,
+    destroy
 }
