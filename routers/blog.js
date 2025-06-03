1b6f6330f57b935cc18086f516944238be0fe5a6
Robby94gtr <robertogramazio94@gmail.com>
2025-06-03 18:01:57 +0200

    [UPD] Added destroy route

--- routers/blog.js
+++ routers/blog.js
@@ -8,5 +8,7 @@ const blogController = require("../controllers/blogController");
 router.get('/', blogController.index)
 // rotta per lo show
 router.get('/:id', blogController.show)
+// rotta per la destroy
+router.delete('/:id', blogController.destroy)
 
 module.exports = router;
